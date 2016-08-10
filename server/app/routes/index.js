ddd3ec6952ad349b103cbb3307dc8524b693188a
Gabe Rodriguez <grod220@gmail.com>
2016-08-10 17:27:36 -0400

    working nodemailer

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,6 +3,7 @@ var router = require('express').Router();
 module.exports = router;
 
 router.use('/members', require('./members'));
+router.use('/mailer', require('./mailer'));
 
 // Make sure this is after all of
 // the registered routes!
