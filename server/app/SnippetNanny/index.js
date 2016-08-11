6ee5e3e6b70ec3e8553fe4025aa86b5994ab6ef3
Gabe Rodriguez <grod220@gmail.com>
2016-08-11 15:32:09 -0400

    investigate slackbot

--- server/app/SnippetNanny/index.js
+++ server/app/SnippetNanny/index.js
@@ -20,18 +20,13 @@ function initialReminder (orgId) {
           pass: creds.snippetGmail.password
       }
   };
-
-  // create reusable transporter object using the default SMTP transport
   var transporter = nodemailer.createTransport(poolConfig);
-
-  // setup e-mail data with unicode symbols
   var mailOptions = {
       from: '"Snippet Nanny 👵" <snippettool@gmail.com>',
       to: 'grod220@gmail.com', // list of receivers --- TRUE USEREMAIL
       subject: '✂ Snippet & ship it ✂',
       html: '<p>Hey ho team,</p><p>Do some great stuff this week? <b>Submit your snippets</b> by the end of the day! The world wants to see...</p><img src="https://media.giphy.com/media/zfCCnYiOyihos/giphy.gif">'
   };
-  // send mail with defined transport object
   transporter.sendMail(mailOptions, function(error, info){
       if(error){
           return console.log(error);
@@ -53,7 +48,34 @@ function initialReminder (orgId) {
 }
 
 function finalCall (orgId) {
+  // Grab all users who have not submitted any snippets
+  // check to see what their notification preferences are
 
+  // for.Each(user)
+    // IF EMAIL -------------
+  var poolConfig = {
+      pool: true,
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // use SSL
+      auth: {
+          user: creds.snippetGmail.email,
+          pass: creds.snippetGmail.password
+      }
+  };
+  var transporter = nodemailer.createTransport(poolConfig);
+  var mailOptions = {
+      from: '"Snippet Nanny 👵" <snippettool@gmail.com>',
+      to: 'grod220@gmail.com', // list of receivers --- TRUE USEREMAIL
+      subject: 'T-minus 60mins to Snippet... ✂',
+      html: '<p>Anything? anything!? Time is running out to submit your weeklies.</p><img src="https://media.giphy.com/media/qq7ef70oHLoAM/giphy.gif" height="150" width="150">,'
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          return console.log(error);
+      }
+      console.log('Message sent: ' + info.response);
+  });
 
 
 }
@@ -66,14 +88,14 @@ function finalCall (orgId) {
 
 // https://www.npmjs.com/package/cron
 new CronJob({
-  cronTime: '*/5 * * * * *', // 00 00 12 * * 3 = every Wednesday @ 12pm // pull actual due date
+  cronTime: '00 00 12 * * 3', // 00 00 12 * * 3 = every Wednesday @ 12pm // pull actual due date
   onTick: initialReminder,
   start: true,
   timeZone: 'America/New_York'
 });
 
 new CronJob({
-  cronTime: '* * * * * 3', // pull actual due date + 5 hours
+  cronTime: '00 00 17 * * 3', // pull actual due date + 5 hours
   onTick: finalCall,
   start: true,
   timeZone: 'America/New_York'
