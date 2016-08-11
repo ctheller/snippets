a00bcbcfbfbc51b78d84c15b079e9cf1582e4491
Chris Heller <ctheller12@gmail.com>
2016-08-11 19:03:17 -0400

    can add users to DB

--- browser/js/login/login.js
+++ browser/js/login/login.js
@@ -8,21 +8,10 @@ app.config(function ($stateProvider) {
 
 });
 
-app.controller('LoginCtrl', function ($scope, AuthService, $state) {
+app.controller('LoginCtrl', function ($scope, Auth) {
 
-    $scope.login = {};
-    $scope.error = null;
-
-    $scope.sendLogin = function (loginInfo) {
-
-        $scope.error = null;
-
-        AuthService.login(loginInfo).then(function () {
-            $state.go('home');
-        }).catch(function () {
-            $scope.error = 'Invalid login credentials.';
-        });
-
+    $scope.googleLogin = function(){
+        Auth.$signInWithRedirect('google');
     };
 
 });
