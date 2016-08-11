1dd42ce85eb1c6e15b0a00f9076489219811249f
Chris Heller <ctheller12@gmail.com>
2016-08-11 14:29:32 -0400

    working on sockets for sequelize version

---
+++ browser/js/common/directives/snippet/snippet.js
@@ -0,0 +1,29 @@
+app.directive('snippet', function ($rootScope, AuthService, AUTH_EVENTS, $state, socketio) {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/common/directives/snippet/snippet.html',
+        scope: {
+        	readonly: '=?',
+        	user: '=',
+            data: '=ngModel',
+            snippetId: '='
+        },
+        link: function(scope, element, attributes) {
+            //need to put snippetId on the scope
+            if (!readonly) {
+                socketio.emit('joinSnippetRoom', scope.snippetId);
+            }
+
+            //need a function that watches snippet contents
+            scope.$watch('data', function(oldValue, newValue) {
+                if (newValue) {
+                    socketio.emit('edit', {snippetId: snippetId, changes: newValue})
+                }
+            });
+
+            socketio.on('edited', function(data){
+                scope.data = data;
+            })
+        }
+    };
+});
