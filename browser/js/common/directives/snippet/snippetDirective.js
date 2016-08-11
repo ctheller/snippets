b9b83d0392bba38232b2afc633e49d1e00dca308
Gabe Rodriguez <grod220@gmail.com>
2016-08-11 18:11:50 -0400

    merging to new master

--- browser/js/common/directives/snippet/snippetDirective.js
+++ browser/js/common/directives/snippet/snippetDirective.js
@@ -1,4 +1,4 @@
-app.directive('snippet', function ($rootScope, AuthService, AUTH_EVENTS, $state, socketio) {
+app.directive('snippet', function ($rootScope, AuthService, AUTH_EVENTS, $state) {
     return {
         restrict: 'E',
         templateUrl: 'js/common/directives/snippet/snippet.html',
@@ -9,21 +9,6 @@ app.directive('snippet', function ($rootScope, AuthService, AUTH_EVENTS, $state,
             snippetId: '='
         },
         link: function(scope, element, attributes) {
-            // need to put snippetId on the scope
-            // if (!readonly) {
-            //     socketio.emit('joinSnippetRoom', scope.snippetId);
-            // }
-
-            // //need a function that watches snippet contents
-            // scope.$watch('data', function(oldValue, newValue) {
-            //     if (newValue) {
-            //         socketio.emit('edit', {snippetId: snippetId, changes: newValue})
-            //     }
-            // });
-
-            // socketio.on('edited', function(data){
-            //     scope.data = data;
-            // })
         }
     };
 });
