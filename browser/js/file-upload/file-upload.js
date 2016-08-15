dd05b81b09893edd7fc0d6d39a1c395928185635
Chris Heller <ctheller12@gmail.com>
2016-08-14 20:44:15 -0400

    some minor adjustments

--- browser/js/file-upload/file-upload.js
+++ browser/js/file-upload/file-upload.js
@@ -1,5 +1,9 @@
 app.controller('MyCtrl', function ($scope, Upload, $timeout, $rootScope) {
 
+    $scope.clear = function(){
+        $scope.result = null;
+    }
+
     $scope.upload = function (dataUrl) {
 
         if (!$rootScope.user) {
@@ -18,52 +22,15 @@ app.controller('MyCtrl', function ($scope, Upload, $timeout, $rootScope) {
         uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
           function(snapshot) {
             // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
-            switch (snapshot.state) {
-              case firebase.storage.TaskState.PAUSED: // or 'paused'
-                console.log('Upload is paused');
-                break;
-              case firebase.storage.TaskState.RUNNING: // or 'running'
-                console.log('Upload is running');
-                break;
-            }
+            console.log("uploading");
           }, function(error) {
-          switch (error.code) {
-            case 'storage/unauthorized':
-              // User doesn't have permission to access the object
-              break;
-
-            case 'storage/canceled':
-              // User canceled the upload
-              break;
-
-            case 'storage/unknown':
-              // Unknown error occurred, inspect error.serverResponse
-              break;
-          }
+            $scope.errorMsg = error.code;
         }, function() {
           // Upload completed successfully, now we can get the download URL
           var downloadURL = uploadTask.snapshot.downloadURL;
+          $scope.result = downloadURL;
           firebase.database().ref().child('users').child(name).child('photoUrl').set(downloadURL);
         });
 
-
-        // Upload.upload({
-        //     url: 'gs://snippets-2f32c.appspot.com',
-        //     data: {
-        //         file: Upload.dataUrltoBlob(dataUrl, name)
-        //     },
-        // }).then(function (response) {
-        //     console.log(response);
-        //     $timeout(function () {
-        //         $scope.result = response.data;
-        //     });
-        // }, function (response) {
-        //     if (response.status > 0) $scope.errorMsg = response.status 
-        //         + ': ' + response.data;
-        // }, function (evt) {
-        //     $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
-        // });
     }
 });
