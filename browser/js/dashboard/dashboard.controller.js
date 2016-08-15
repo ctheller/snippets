053354783bfeb17d82ad88d112bd13a77add9012
Chris Heller <ctheller12@gmail.com>
2016-08-15 15:25:13 -0400

    another small change

--- browser/js/dashboard/dashboard.controller.js
+++ browser/js/dashboard/dashboard.controller.js
@@ -1,45 +1,57 @@
-app.controller('DashboardCtrl', function($rootScope, $scope, $mdDialog, MdHelpers, AUTH_EVENTS) {
+app.controller('DashboardCtrl', function($rootScope, $scope, $mdDialog, MdHelpers, AUTH_EVENTS, Snippet) {
    
-    $rootScope.$on(AUTH_EVENTS.loginSuccess, function(){
-
-        $scope.newSnippet = {};
-        $scope.showSnippetForm = function(ev) {
-            $mdDialog.show({
-                controller: MdHelpers.dialogCtrl,
-                templateUrl: 'js/dashboard/new-snippet-form.html',
-                parent: angular.element(document.body),
-                targetEvent: ev,
-                clickOutsideToClose: true
-            });
-        };
-        $scope.draggables = [
-            {icon: 'people'}, {icon: 'person'}
-        ];
-        $scope.card = true;
-        $scope.dragged = [];
+    var setScope = function(){
         $scope.teamSnippetIds = $rootScope.user.snippets.asTeamMember ? Object.keys($rootScope.user.snippets.asTeamMember) : [];
         $scope.collabSnippetIds = $rootScope.user.snippets.asCollaborator ? Object.keys($rootScope.user.snippets.asCollaborator) : [];
         $scope.reportSnippetIds = $rootScope.user.snippets.asManager ? Object.keys($rootScope.user.snippets.asManager) : [];
-        $scope.collabAndTeamSnippetIds = _.union($scope.collabSnippetIds, $scope.teamSnippetIds);
+        $scope.collabAndTeamSnippetIds = _.union($scope.collabSnippetIds, $scope.teamSnippetIds);  
+    }
 
+
+    $rootScope.$on(AUTH_EVENTS.loginSuccess, function(){
+        setScope();
         $rootScope.userFirebaseObj.$watch(function(){
-            $scope.teamSnippetIds = $rootScope.user.snippets.asTeamMember ? Object.keys($rootScope.user.snippets.asTeamMember) : [];
-            $scope.collabSnippetIds = $rootScope.user.snippets.asCollaborator ? Object.keys($rootScope.user.snippets.asCollaborator) : [];
-            $scope.reportSnippetIds = $rootScope.user.snippets.asManager ? Object.keys($rootScope.user.snippets.asManager) : [];
-            $scope.collabAndTeamSnippetIds = _.union($scope.collabSnippetIds, $scope.reportSnippetIds);
+            setScope();
         })
-
-
-        $scope.toggle = function() {
-            $mdSidenav('right').toggle();
-            if ($mdSidenav('right').isOpen()) $rootScope.$emit('open');
-            else $rootScope.$emit('close');
-        };
-
     });
+
+    if ($scope.user) {
+        setScope();
+        $rootScope.userFirebaseObj.$watch(function(){
+            setScope();
+        })
+    }
+
+
+
+    $scope.card = true;
+    $scope.dragged = [];
+
+    $scope.toggle = function() {
+        $mdSidenav('right').toggle();
+        if ($mdSidenav('right').isOpen()) $rootScope.$emit('open');
+        else $rootScope.$emit('close');
+    };
+
+    $scope.newSnippet = {};
+
+    $scope.showSnippetForm = function(ev) {
+        $mdDialog.show({
+            controller: MdHelpers.dialogCtrl,
+            templateUrl: 'js/dashboard/new-snippet-form.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true
+        });
+    };
+
+    $scope.draggables = [
+        {icon: 'people'}, {icon: 'person'}
+    ];
 
     $scope.createNewSnippet = function (e, ui) {
         var snippetCopyId = ui.draggable.scope().id;
         Snippet.duplicateAsTemplate(snippetCopyId);
     }
+
 });
