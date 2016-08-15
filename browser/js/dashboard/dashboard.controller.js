efcf89a00a0861905f7c4e4b5e6c55cfcd11c3ee
yungtiec <tctammychu@gmail.com>
2016-08-15 17:07:58 -0400

    add dashboard view for report (not a manager)

--- browser/js/dashboard/dashboard.controller.js
+++ browser/js/dashboard/dashboard.controller.js
@@ -1,31 +1,29 @@
 app.controller('DashboardCtrl', function($rootScope, $scope, $mdDialog, MdHelpers, AUTH_EVENTS, Snippet) {
-   
-    var setScope = function(){
+
+    var setScope = function() {
         $scope.teamSnippetIds = $rootScope.user.snippets.asTeamMember ? Object.keys($rootScope.user.snippets.asTeamMember) : [];
         $scope.collabSnippetIds = $rootScope.user.snippets.asCollaborator ? Object.keys($rootScope.user.snippets.asCollaborator) : [];
         $scope.reportSnippetIds = $rootScope.user.snippets.asManager ? Object.keys($rootScope.user.snippets.asManager) : [];
-        $scope.collabAndTeamSnippetIds = _.union($scope.collabSnippetIds, $scope.teamSnippetIds);  
+        $scope.collabAndTeamSnippetIds = _.union($scope.collabSnippetIds, $scope.teamSnippetIds);
+        $scope.isManager = false;
+        if ($rootScope.user['reports']) {
+            $scope.isManager = true;
+        }
     }
 
-
-    $rootScope.$on(AUTH_EVENTS.loginSuccess, function(){
+    $rootScope.$on(AUTH_EVENTS.loginSuccess, function() {
         setScope();
-        $rootScope.userFirebaseObj.$watch(function(){
+        $rootScope.userFirebaseObj.$watch(function() {
             setScope();
         })
     });
 
     if ($scope.user) {
         setScope();
-        $rootScope.userFirebaseObj.$watch(function(){
+        $rootScope.userFirebaseObj.$watch(function() {
             setScope();
         })
     }
-
-
-
-    $scope.card = true;
-    $scope.dragged = [];
 
     $scope.toggle = function() {
         $mdSidenav('right').toggle();
@@ -46,10 +44,10 @@ app.controller('DashboardCtrl', function($rootScope, $scope, $mdDialog, MdHelper
     };
 
     $scope.draggables = [
-        {icon: 'people'}, {icon: 'person'}
+        { icon: 'people' }, { icon: 'person' }
     ];
 
-    $scope.createNewSnippet = function (e, ui) {
+    $scope.createNewSnippet = function(e, ui) {
         var snippetCopyId = ui.draggable.scope().id;
         Snippet.duplicateAsTemplate(snippetCopyId);
     }
