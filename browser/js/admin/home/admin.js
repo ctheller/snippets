266bac04b3f3d4c55188c8ae93c37d91d8aa7c20
Chris Heller <ctheller12@gmail.com>
2016-08-22 16:04:56 -0400

    added dueDate

--- browser/js/admin/home/admin.js
+++ browser/js/admin/home/admin.js
@@ -1,9 +1,20 @@
-app.controller('adminCtrl', function(Users, $rootScope) {
+app.controller('adminCtrl', function($scope, Users, $rootScope, $firebaseObject) {
     var Users = {};
     var Snippets = {};
+    $scope.day = {};
 
     // create a reference to the database node where we will store our data
     var ref_users = firebase.database().ref("users");
+    var ref_org = firebase.database().ref("organizations").child($rootScope.user.organization);
+    $firebaseObject(ref_org).$loaded().then(function(){
+        $scope.day.number = $scope.organization.dueDay.day;
+        $scope.day.hour = $scope.organization.dueDay.time;
+    });
+
+
+    $scope.updateDueDate = function(){
+
+    }
 
 });
 
