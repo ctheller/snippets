fdaa5bf2759cada1d5cddeb41d45e13f7087ae46
Gabe Rodriguez <grod220@gmail.com>
2016-08-24 11:05:22 -0400

    resizing fixed

--- browser/js/graphing/graphing.js
+++ browser/js/graphing/graphing.js
@@ -17,42 +17,8 @@ app.config(function($stateProvider) {
 });
 
 app.controller('GraphCtrl', function($scope, $rootScope, orgJson, getSnips) {
-
-    $scope.notifyServiceOnChage = function(){
-         console.log($scope.windowHeight);
-      };
-
-    // $http.get('/api/orgtree/' + $rootScope.user.organization).then(function(result) {
-        $scope.orgJson = orgJson;
-    // });
-    // console.log(getSnips.data);
-    $scope.cardStyle;
-
-    $scope.$on('$viewContentLoaded', function(){
-        let widthStr = window.getComputedStyle(document.getElementsByClassName("widthToMeasure")[0], null).width;
-        let widthToPass = (Number(widthStr.slice(0,-2))) + 'px';
-        let heightToPass = window.getComputedStyle(document.getElementsByClassName("widthToMeasure")[0], null).height;
-        $scope.canvasStyle = {width: widthToPass, height: heightToPass};
-        setTimeout(function() {
-            WordCloud(document.getElementById('wordCloud'), { list: getSnips.data, backgroundColor: 'transparent' } );
-            let newHeight = (Number(heightToPass.slice(0,-2)) + 5) + 'px';
-            $scope.cardStyle = {'height': newHeight};
-            $scope.$digest();
-        }, 100);
-
-    //     // $scope.getWeekSnippets($rootScope.user.organization)
-    //     // .then(function(result) {
-    //     //    console.log(result);
-    //     // });
-    //     // WordCloud(document.getElementById('wordCloud'), { list: $scope.tags } );
-    });
-
-    // $scope.tags = [['foo', 12], ['bar', 6], ['Gabe', 80]];
-    // var tree = {};
-    // $rootScope.users.forEach(function(user){
-    //  if (!user.manager)
-    //  if (tree.hasOwnProperty(user.manager)) tree[user.manager].push(user.$id)
-    // })
+    $scope.orgJson = orgJson;
+    $scope.listData = getSnips.data;
 });
 
 
@@ -67,21 +33,33 @@ app.directive('resize', function ($window) {
                 'w': window.innerWidth
             };
         }, function (newValue, oldValue) {
-            console.log(newValue, oldValue);
+
+            let widthStr = window.getComputedStyle(document.getElementsByClassName("widthToMeasure")[0], null).width;
+            let widthToPass = (Number(widthStr.slice(0,-2))) + 'px';
+            let heightToPass = window.getComputedStyle(document.getElementsByClassName("widthToMeasure")[0], null).height;
+            scope.canvasStyle = {width: widthToPass, height: heightToPass};
+            setTimeout(function() {
+                WordCloud(document.getElementById('wordCloud'), { list: scope.listData, backgroundColor: 'transparent' } );
+                let newHeight = (Number(heightToPass.slice(0,-2)) + 5) + 'px';
+                scope.cardStyle = {'height': newHeight};
+                scope.$digest();
+            }, 100);
+
+            // console.log(newValue);
             scope.windowHeight = newValue.h;
             scope.windowWidth = newValue.w;
 
-            scope.resizeWithOffset = function (offsetH) {
-                scope.$eval(attr.notifier);
-                return {
-                    'height': (newValue.h - offsetH) + 'px'
-                };
-            };
+            // scope.resizeWithOffset = function (offsetH) {
+            //     scope.$eval(attr.notifier);
+            //     return {
+            //         'height': (newValue.h - offsetH) + 'px'
+            //     };
+            // };
 
         }, true);
 
         w.bind('resize', function () {
             scope.$apply();
         });
-    }
+    };
 });
